be1b02f7fb8e9a1cf5a8103ee74efe1612eb7552
maikoncorrea <maikonacorrea@gmail.com>
2024-05-11 20:47:29 -0300

    database fix

--- backend/data/database.js
+++ backend/data/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 module.exports = function connectDatabase() {
-  mongoose.connect(`mongodb+srv://maikonacorrea:${process.env.PASSWORD_MONGODB}@cluster0.dsxeka9.mongodb.net/newsexplorer?retryWrites=true&w=majority`);
-
+  mongoose.connect(`mongodb+srv://maikonacorrea:${process.env.PASSWORD_MONGODB}@cluster0.dsxeka9.mongodb.net/newsexplorer?retryWrites=true&w=majority&appName=Cluster0`);
   console.log('Database Conected!');
 };
